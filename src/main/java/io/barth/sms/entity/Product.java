015e9429eaf92c202533c3302b8b2ee783e3dd15
WaveGuide1 <barthalaekwe@gmail.com>
2024-03-28 19:09:23 +0100

    security user entity

--- src/main/java/io/barth/sms/entity/Product.java
+++ src/main/java/io/barth/sms/entity/Product.java
@@ -8,14 +8,23 @@ import lombok.EqualsAndHashCode;
 @Data
 @Entity
 @EqualsAndHashCode(callSuper = true)
+@Table(name = "product")
 public class Product extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "product_name")
     private String productName;
+
+    @Column(name = "description")
     private String description;
-    private int quantity;
-    private int price;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "price")
+    private Integer price;
 
 }
